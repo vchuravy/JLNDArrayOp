533d9a7fa79eb4f3fca0ac47dc86ab1a1ac24d18
Valentin Churavy <v.churavy@gmail.com>
2016-08-10 05:46:48 +0900

    refactor jlrtc

--- jlrtc.cc
+++ jlrtc.cc
@@ -3,49 +3,37 @@
 namespace mxnet {
 
 JLRtc::JLRtc(const std::string& name,
-             std::vector<NDArray> const& input,
-             std::vector<NDArray> const& output,
-             char* ptx) {
+          std::vector<NDArray> const& args,
+          char* ptx,
+          unsigned int grid_dim_X,
+          unsigned int grid_dim_Y,
+          unsigned int grid_dim_Z,
+          unsigned int block_dim_X,
+          unsigned int block_dim_Y,
+          unsigned int block_dim_Z) {
   name_ = name;
   ptx_ = ptx;
 
-  for (auto& ndarray: input) {
-    in_dtypes_.push_back(ndarray.dtype());
-    in_shapes_.push_back(ndarray.shape());
-  }
-
-  for (auto& ndarray: output) {
-    out_dtypes_.push_back(ndarray.dtype());
-    out_shapes_.push_back(ndarray.shape());
+  for (auto& ndarray: args) {
+    dtypes_.push_back(ndarray.dtype());
+    shapes_.push_back(ndarray.shape());
   }
 
 }
 
-void JLRtc::push(std::vector<NDArray> const& input,
-                 std::vector<NDArray> const& output,
-                 unsigned int grid_dim_X,
-                 unsigned int grid_dim_Y,
-                 unsigned int grid_dim_Z,
-                 unsigned int block_dim_X,
-                 unsigned int block_dim_Y,
-                 unsigned int block_dim_Z) {
-  CHECK_EQ(in_dtypes_.size(), input.size());
-  CHECK_EQ(out_dtypes_.size(), output.size());
-  CHECK(output.size());
+void JLRtc::verify(std::vector<NDArray> const& ndargs) {
+  CHECK_EQ(dtypes_.size(), ndargs.size());
+  CHECK(ndargs.size());
 
-  for (size_t i = 0; i < input.size(); ++i) {
-    CHECK_EQ(in_dtypes_[i], input[i].dtype());
-    CHECK_EQ(in_shapes_[i], input[i].shape());
+  for (size_t i = 0; i < ndargs.size(); ++i) {
+    CHECK_EQ(dtypes_[i], ndargs[i].dtype());
+    CHECK_EQ(shapes_[i], ndargs[i].shape());
   }
+}
 
-  for (size_t i = 0; i < output.size(); ++i) {
-    CHECK_EQ(out_dtypes_[i], output[i].dtype());
-    CHECK_EQ(out_shapes_[i], output[i].shape());
-  }
-
+CUfunction JLRtc::getFunc(int dev_id) {
   cudaError_enum err;
   CUfunction func;
-  int dev_id = output[0].ctx().dev_id;
   if (func_.find(dev_id) != func_.end()) {
         func = func_[dev_id];
   } else {
@@ -58,19 +46,18 @@ void JLRtc::push(std::vector<NDArray> const& input,
     func_[dev_id] = func;
   }
 
-  auto op = [this, func, input, output,
-             grid_dim_X, grid_dim_Y, grid_dim_Z,
-             block_dim_X, block_dim_Y, block_dim_Z](RunContext rctx) {
-    std::vector<CUdeviceptr> args;
-    for (CUdeviceptr dptr: convert(input)) args.push_back(dptr);
-    for (CUdeviceptr dptr: convert(output)) args.push_back(dptr);
+  return func;
+}
 
-    cudaError_enum err;
+void JLRtc::launch(CUfunction func, std::vector<NDArray> const& ndargs, RunContext& rctx) {
+    std::vector<CUdeviceptr> args = convert(ndargs);
+
     cudaError_t cuerr;
+    cudaError_enum err;
 
     CHECK_EQ(err = cuLaunchKernel(func,
-                            grid_dim_X, grid_dim_Y, grid_dim_Z,
-                            block_dim_X, block_dim_Y, block_dim_Z,
+                            grid_dim_X_, grid_dim_Y_, grid_dim_Z_,
+                            block_dim_X_, block_dim_Y_, block_dim_Z_,
                             0, rctx.get_stream<mshadow::gpu>()->stream_,
                             (void**) args.data(), 0), CUDA_SUCCESS) << "CudaError: " << err;
 
@@ -80,11 +67,6 @@ void JLRtc::push(std::vector<NDArray> const& input,
     for (auto dptr: args) {
       CHECK_EQ(err = cuMemFree(dptr), CUDA_SUCCESS) << "CudaError: " << err;
     }
-  };
-
-  std::vector<Engine::VarHandle> var_in, var_out;
-  for (auto& i : input) var_in.push_back(i.var());
-  for (auto& i : output) var_out.push_back(i.var());
-  Engine::Get()->PushSync(op, output[0].ctx(), var_in, var_out);
 }
+
 }
--- jlrtc.h
+++ jlrtc.h
@@ -11,28 +11,32 @@ namespace mxnet {
 class JLRtc {
   public:
     JLRtc(const std::string& name,
-          std::vector<NDArray> const& input,
-          std::vector<NDArray> const& output,
-          char* ptx);
+          std::vector<NDArray> const& args,
+          char* ptx,
+          unsigned int grid_dim_X,
+          unsigned int grid_dim_Y,
+          unsigned int grid_dim_Z,
+          unsigned int block_dim_X,
+          unsigned int block_dim_Y,
+          unsigned int block_dim_Z);
 
-    void push(std::vector<NDArray> const& input,
-              std::vector<NDArray> const& output,
-              unsigned int grid_dim_X,
-              unsigned int grid_dim_Y,
-              unsigned int grid_dim_Z,
-              unsigned int block_dim_X,
-              unsigned int block_dim_Y,
-              unsigned int block_dim_Z);
+    void verify(std::vector<NDArray> const& ndargs);
+    CUfunction getFunc(int dev_id);
+    void launch(CUfunction func, std::vector<NDArray> const& ndargs, RunContext& rctx);
 
   private:
     std::string name_;
     char* ptx_;
-    std::vector<int> in_dtypes_;
-    std::vector<int> out_dtypes_;
-    std::vector<TShape> in_shapes_;
-    std::vector<TShape> out_shapes_;
+    std::vector<int> dtypes_;
+    std::vector<TShape> shapes_;
     std::unordered_map<int, CUmodule> module_;
     std::unordered_map<int, CUfunction> func_;
+    unsigned int grid_dim_X_;
+    unsigned int grid_dim_Y_;
+    unsigned int grid_dim_Z_;
+    unsigned int block_dim_X_;
+    unsigned int block_dim_Y_;
+    unsigned int block_dim_Z_;
 };
 }
 #endif
